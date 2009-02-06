7b8b32a303153e838bf012b8d3a1546b762a782a
Tom Jenkinson <tom.jenkinson@redhat.com>
2009-02-06 22:35:50 +0000

    Updated to support the new response struct
    git-svn-id: http://anonsvn.jboss.org/repos/blacktie/trunk@171 aa7494b7-e75f-0410-880c-f61b6724bf28

--- ejb-connector-example/src/main/java/org/jboss/blacktie/example/ejb/BarServiceBean.java
+++ ejb-connector-example/src/main/java/org/jboss/blacktie/example/ejb/BarServiceBean.java
@@ -41,7 +41,7 @@ public class BarServiceBean extends AbstractBlacktieService implements SessionBe
 		Buffer data = svcinfo.getData();
 		Buffer buffer = new X_OCTET(data.getSize());
 		buffer.setData(data.getData());
-		Response response = new Response(buffer);
+		Response response = new Response((short) 0, 0, buffer, 0);
 		return response;
 	}
 
