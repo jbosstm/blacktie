86aa2b3febbe83fd5f9ba9b7aed0fd4562c2994c
Mike Musgrove <mmusgrov@redhat.com>
2009-02-11 16:33:01 +0000

    Placeholder for transacion test utilities - in particular for building an library for a dummy resource manager.
    BLACKTIE-113
    
    
    git-svn-id: http://anonsvn.jboss.org/repos/blacktie/trunk@187 aa7494b7-e75f-0410-880c-f61b6724bf28

---
+++ tx-test-utilities/src/main/cpp/rm.c
@@ -0,0 +1,31 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2009, Red Hat Middleware LLC, and others contributors as indicated
+ * by the @authors tag. All rights reserved.
+ * See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU Lesser General Public License, v. 2.1.
+ * This program is distributed in the hope that it will be useful, but WITHOUT A
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License,
+ * v.2.1 along with this distribution; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ */
+#include "xa.h"
+
+static int fn1(char *a, int i, long l) { return 0; }
+static int fn2(XID *x, int i, long l) { return 0; }
+static int fn3(XID *xid, long l1, int i, long l2) { return 0; }
+static int fn4(int *ip1, int *ip2, int i, long l) { return 0; }
+
+struct xa_switch_t testxasw = { "DummyRM", 0L, 0, fn1, fn1, /* open and close */
+        fn2, fn2, fn2, fn2, fn2, /*start, end, rollback, prepare, commit */
+        fn3, /* recover */
+        fn2, /* forget */
+        fn4 /* complete */
+};
+
