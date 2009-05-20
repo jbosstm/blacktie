c8c47cd820ebe3a04ba8d98263557a12e795ab84
Tom Jenkinson <tom.jenkinson@redhat.com>
2009-05-20 17:35:40 +0000

    Initial JMS transport
    git-svn-id: http://anonsvn.jboss.org/repos/blacktie/trunk@754 aa7494b7-e75f-0410-880c-f61b6724bf28

---
+++ jatmibroker-xatmi/src/main/java/org/jboss/blacktie/jatmibroker/transport/jms/TransportImpl.java
@@ -0,0 +1,58 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2008, Red Hat, Inc., and others contributors as indicated
+ * by the @authors tag. All rights reserved.
+ * See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU Lesser General Public License, v. 2.1.
+ * This program is distributed in the hope that it will be useful, but WITHOUT A
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License,
+ * v.2.1 along with this distribution; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ */
+package org.jboss.blacktie.jatmibroker.transport.jms;
+
+import org.jboss.blacktie.jatmibroker.JAtmiBrokerException;
+import org.jboss.blacktie.jatmibroker.transport.Receiver;
+import org.jboss.blacktie.jatmibroker.transport.Sender;
+import org.jboss.blacktie.jatmibroker.transport.Transport;
+import org.omg.CosNaming.NamingContextPackage.CannotProceed;
+import org.omg.CosNaming.NamingContextPackage.NotFound;
+import org.omg.CosTransactions.TransactionFactory;
+
+public class TransportImpl implements Transport {
+
+	TransportImpl() {
+	}
+
+	public void close() {
+	}
+
+	public TransactionFactory getTransactionFactory(
+			String transactionManagerServiceName) throws NotFound,
+			CannotProceed, org.omg.CosNaming.NamingContextPackage.InvalidName {
+		return null;
+	}
+
+	public Sender getSender(String serviceName) throws JAtmiBrokerException {
+		return new SenderImpl();
+	}
+
+	public Sender createSender(String callback_ior) {
+		return new SenderImpl();
+	}
+
+	public Receiver createReceiver(String serviceName)
+			throws JAtmiBrokerException {
+		return new ReceiverImpl();
+	}
+
+	public Receiver createReceiver() throws JAtmiBrokerException {
+		return new ReceiverImpl();
+	}
+}
