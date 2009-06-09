ab58074e72391511262855edf04d2746dc224a0b
Tom Jenkinson <tom.jenkinson@redhat.com>
2009-06-09 07:08:56 +0000

    git-svn-id: http://anonsvn.jboss.org/repos/blacktie/trunk@846 aa7494b7-e75f-0410-880c-f61b6724bf28

--- atmibroker-stomp-administration/src/test/java/org/jboss/blacktie/administration/BlacktieStompAdministrationService.java
+++ atmibroker-stomp-administration/src/test/java/org/jboss/blacktie/administration/BlacktieStompAdministrationService.java
@@ -15,7 +15,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  * MA  02110-1301, USA.
  */
-package org.jboss.blacktie.jatmibroker.ejb.connector.ejb;
+package org.jboss.blacktie.administration;
 
 import junit.framework.TestCase;
 
