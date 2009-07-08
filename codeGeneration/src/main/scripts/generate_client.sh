1fe834fcd2887f47dc6dc1f676aee7b9e3a341e5
Mike Musgrove <mmusgrov@redhat.com>
2009-07-08 16:14:55 +0000

    BLACKTIE-180
    git-svn-id: http://anonsvn.jboss.org/repos/blacktie/trunk@1007 aa7494b7-e75f-0410-880c-f61b6724bf28

--- codeGeneration/src/main/scripts/generate_client.sh
+++ codeGeneration/src/main/scripts/generate_client.sh
@@ -1,1 +1,1 @@
-java org.apache.tools.ant.launch.Launcher -f $BLACKTIE_BIN_DIR/bin/generate-executables.xml -Dconfiguration.type=debug -Dbasedir=. -Dtoolchain.compiler=gcc client $1
+java org.apache.tools.ant.launch.Launcher -f $BLACKTIE_BIN_DIR/bin/generate-executables.xml -Dconfiguration.type=debug -Dbasedir=. -Dtoolchain.compiler=gcc client "$@"
