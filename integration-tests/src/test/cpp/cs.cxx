5be4a40867cf02ea4ad440943208167fb136ac1f
Mike Musgrove <mmusgrov@redhat.com>
2009-11-08 17:22:29 +0000

    Add a simple integration test harness for running C based client/server tests across process boundaries.
    BLACKTIE-217
    
    
    git-svn-id: http://anonsvn.jboss.org/repos/blacktie/trunk@1456 aa7494b7-e75f-0410-880c-f61b6724bf28

---
+++ integration-tests/src/test/cpp/cs.cxx
@@ -0,0 +1,410 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2009, Red Hat, Inc., and others contributors as indicated
+ * by the @authors tag. All rights reserved.
+ * See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU Lesser General Public License, v. 2.1.
+ * This program is distributed in the hope that it will be useful, but WITHOUT A
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License,
+ * v.2.1 along with this distribution; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ */
+#include "AtmiBrokerServerControl.h"
+
+#include <stdlib.h>
+
+#include "xatmi.h"
+#include "userlogc.h"
+#include "string.h"
+
+#include "ace/Thread.h"
+#include "ace/Synch.h"
+
+/*
+ g++ -I$HOME/blacktie/trunk/atmibroker-xatmi/target/cxx/compile/include -I$HOME/blacktie/trunk/atmibroker-xatmi/target/classes/include -lACE -L/home/mmusgrov/blacktie/releases/blacktie-1.0-MR6-SNAPSHOT/lib -latmibroker-xatmi cs.c -o cs
+ */
+
+#if 1
+
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "tx.h"
+
+static ACE_Mutex mutex_;
+const char *MSG1 = "CLIENT REQUEST		";
+const char *MSG2 = "PAUSE - CLIENT REQUEST";
+
+// data type for controlling the work done by each thread
+typedef struct thr_arg {
+	int failonerror;
+	const char *data;
+	const char *msg;
+	const char *svc;
+	const char *sndtype;
+	const char *rcvtype;
+	long flags;
+	int expect;
+	int result;
+} thr_arg_t;
+
+// arbitray subtype for testing C type buffers
+typedef struct subtype {
+	char data[64];
+	int id;
+	char op;
+} subtype_t;
+
+static void do_assert(int failonerror, int* res, int cond, const char *fmt, ...) {
+	char str[1024];
+	va_list args;
+
+	va_start(args, fmt);
+	vsnprintf(str, 1024, fmt, args);
+	va_end(args);
+
+	if (!cond) {
+		*res = 1;
+		userlogc((char*) "UNSUCCESSFULL assert %s", str);
+		if (failonerror)
+			_exit(1);
+	}
+
+//	userlogc((char*) "successful assert for: cond=%d (%s)", cond, str);
+}
+
+//static int do_tpcall(int failonerror, const char *data, const char *msg, const char *svc, const char *sndtype, long flags, int expect) {
+static int do_tpcall(thr_arg_t *args) {
+	int tpstatus;
+	char *rbuf;
+	char *sbuf;
+	char type[20];
+	char subtype[20];
+	int isSbCType = (strcmp(args->sndtype, X_C_TYPE) == 0); // is send buffer is X_C_TYPE
+	int isRbCType = (strcmp(args->rcvtype, X_C_TYPE) == 0); // is recv buffer is X_C_TYPE
+	long sbufsz = (isSbCType ? 0 : strlen(args->data) + 1);	// api sets the buffer size if X_C_TYPE
+	long rbufsz = (isRbCType ? 0 : 64);
+	int res;
+
+	sbuf = tpalloc((char *) args->sndtype, (isSbCType ? (char*) "sub_type" : NULL), sbufsz);
+	rbuf = tpalloc((char *) args->rcvtype, (isRbCType ? (char*) "sub_type" : NULL), rbufsz);
+
+	do_assert(args->failonerror, &args->result, sbuf != 0, "tpalloc send buf failed tperrno=%d", tperrno);
+	do_assert(args->failonerror, &args->result, rbuf != 0, "tpalloc recv buf failed tperrno=%d", tperrno);
+
+	strcpy(sbuf, args->data);
+	memset(rbuf, 0, rbufsz);
+
+	tptypes(sbuf, type, subtype);
+	userlogc((char *) "sbuf type: %s rbuf type: %s type: %s subtype: %s %d vrs %d",
+		args->sndtype, args->rcvtype, type, subtype, tpstatus, args->expect);
+
+	tpstatus = tpcall((char *) args->svc, sbuf, sbufsz, (char **) &rbuf, &rbufsz, args->flags);
+	res = (tperrno == args->expect ? 0 : 1);
+	if (tpstatus)
+		userlogc((char *) "tpcall returned %d tperrno=%d expect=%d", tpstatus, tperrno, args->expect);
+	do_assert(args->failonerror, &args->result, tperrno == args->expect,
+		"%s: wrong response from tpcall %s %s tpstatus=%d flags=%d expect=%d tperrno=%d",
+		args->msg, args->svc, args->sndtype, tpstatus, args->flags, args->expect, tperrno);
+
+	tpfree(sbuf);
+	tpfree(rbuf);
+
+	return res;
+}
+
+static int lotsofwork(int nthreads, ACE_THR_FUNC tfunc, thr_arg_t* arg) {
+	ACE_thread_t *tids = new ACE_thread_t[nthreads];
+	ACE_hthread_t *handles = new ACE_hthread_t[nthreads];
+	int i;
+
+	for (i = 0; i < nthreads; i++)
+		handles[i] = 0;
+
+	// spawn nthreads threads
+	if (ACE_Thread::spawn_n(tids, // return thread id for each thread
+		nthreads,
+		tfunc, // entry point for new thread
+		(void *) arg,	// args for thread entry point
+		THR_JOINABLE | THR_NEW_LWP,
+		ACE_DEFAULT_THREAD_PRIORITY,
+		0, 0, handles) != (size_t) nthreads) {
+				userlogc("Unable to start request number of threads\n");
+	}
+
+	for (int i = 0; i < nthreads; i++)
+		if (handles[i] != 0)
+			ACE_Thread::join(handles[i]);
+
+	return arg->result;
+}
+
+// thread entry point
+static void* work(void *args)
+{
+	(void) do_tpcall((thr_arg_t *) args);
+	return args;
+}
+
+// another thread entry point
+static int tcnt_ = 0;
+static void* work2(void *args)
+{
+	thr_arg_t *params = (thr_arg_t *) args;
+	char *s1, *s2;
+	int ncalls = 2;
+	int okcalls = 0;
+	int rv;
+
+	s1 = (char *) "BAR";
+	s2 = (char *) "BAR";
+
+	mutex_.acquire();
+	tcnt_ += 1;
+	tpfree(tpalloc((char *) params->sndtype, 0, 10));
+#if 0	/* I've disabled using multiple service since it fails with just one service */
+	if (tcnt_  % 2 == 0)
+		s2 = (char *) "TestTPCall";
+	else
+		s1 = (char *) "TestTPCall";
+#endif
+	mutex_.release();
+
+	ACE_OS::sleep(4);	// yield to ensure that all threads have initialised env (see bug BLACKTIE-211)
+
+	for (int i = 0; i < ncalls; i++) {
+		params->svc = s1;
+		if ((rv = do_tpcall(params)))
+			userlogc((char*) "%s: tpcall %d error: %d", params->svc, i, rv);
+		else
+			okcalls += 1;
+		params->svc = s2;
+		if ((rv = do_tpcall(params)))
+			userlogc((char*) "%s: tpcall %d error: %d", params->svc, i, rv);
+		else
+			okcalls += 1;
+	}
+
+	userlogc("Thread (t) finished %d out of %d calls successful\n", okcalls, ncalls * 2);
+
+	params->result = (okcalls == ncalls * 2);
+	return args;
+}
+
+// XsdValidator is not thread safe
+static int bug211() {
+	thr_arg_t args = {1, MSG1, "bug211: two threads reading env", "BAR", X_OCTET, X_OCTET, 0, 0};
+	return lotsofwork(2, work, &args);
+}
+
+// tpcall should return TPEINVAL if the service name is invalid
+static int bug213() {
+	thr_arg_t args = {1, MSG1, "bug213: NULL service name", NULL, X_OCTET, X_OCTET, 0, TPEINVAL};
+	return do_tpcall(&args);
+}
+
+// tpcall incorrectly returns TPNOTIME whenever the TPNOBLOCK or TPNOTIME flags are specified
+static int bug212a() {
+	// Specifying TPNOTIME means the caller wants to be imune to blocking conditions (such
+	// as no output buffers). Thus if such a condition does not exist the call should succeed as normal.
+	// However if bug 212 is present then the call returns TPNOTIME
+	long flags2 = TPNOTRAN | TPNOTIME;
+	thr_arg_t arg1 = {1, MSG1, "bug212a: TPNOTIME", "BAR", X_OCTET, X_OCTET, flags2, 0};
+
+	return lotsofwork(1, work, &arg1);
+}
+static int bug212b() {
+	// Similarly specifying TPNOBLOCK means that if a blocking condition does exist then the caller
+	// should get the error TPEBLOCK
+	// However if bug 212 is present then the call returns TPNOTIME
+	long flags3 = TPNOTRAN | TPNOBLOCK;
+
+	thr_arg_t args = {1, MSG1, "bug212b: TPNOBLOCK", "BAR", X_OCTET, X_OCTET, flags3, 0};
+
+	return lotsofwork(1, work, &args);
+}
+
+// TPSIGRSTRT flag isn't supported on tpcall
+static int bug214() {
+	thr_arg_t args = {1, MSG1, "bug214: TPSIGRSTRT flag not supported on tpcall", "BAR", X_OCTET, X_OCTET, TPSIGRSTRT, 0};
+	return lotsofwork(1, work, &args);
+}
+
+// tpcall failure with multiple threads
+static int bug215() {
+	thr_arg_t args = {0, MSG1, "bug215: tpcall failure with lots of threads", "BAR", X_OCTET, X_OCTET, 0, 0};
+	return lotsofwork(2, work2, &args);
+}
+
+static int bug216a() {
+	thr_arg_t args = {1, MSG1, "bug216: tp bufs should morph if they're the wrong type", "BAR", X_OCTET, X_C_TYPE, 0, 0};
+	return lotsofwork(1, work, &args);
+}
+
+static int bug216b() {
+	thr_arg_t args = {1, MSG1, "bug216: passing the wrong return buffer type with TPNOCHANGE",
+		"BAR", X_OCTET, X_C_TYPE, TPNOCHANGE, TPEOTYPE};
+	return lotsofwork(1, work, &args);
+}
+
+static int bug217() {
+	thr_arg_t args = {1, MSG1, "bug217: make sure tpurcode works", "BAR", X_OCTET, X_OCTET, 0, 0};
+	(void) lotsofwork(1, work, &args);
+	do_assert(1, &(args.result), tpurcode == 99, "tpurcode should have been 99: tpurcode=%d tperrno=%d", tpurcode, tperrno);
+	return args.result;
+}
+
+static int t9001() {
+	char *buf = (char *) tpalloc((char *) X_C_TYPE, (char*) "sub_type", 10);
+	int res;
+	do_assert(1, &res, buf != 0, "tpalloc with X_C_TYPE and non-zero len: tperrno=%d (spec says size is optional)", tperrno);
+	tpfree(buf);
+	return res;
+}
+
+// sanity check
+static int t0() {
+	thr_arg_t args = {1, MSG1, "ok test", "BAR", X_OCTET, X_OCTET, 0, 0};
+	return do_tpcall(&args);
+}
+
+// tell the server to set a flag on tpreturn (should generate TPESVCERR)
+static int t1() {
+	thr_arg_t args = {1, "T1", "set flag on tpreturn should fail", "BAR", X_OCTET, X_OCTET, TPNOTRAN, TPESVCERR};
+	return do_tpcall(&args);
+}
+static int t2() {
+	thr_arg_t args = {1, "T2", "tell the service to free the the service buffer", "BAR", X_OCTET, X_OCTET, TPNOTRAN, 0};
+	return do_tpcall(&args);
+}
+
+// telling the service to not tpreturn should generate an error
+static int t3() {
+	thr_arg_t args = {1, "T3", "no tpreturn", "BAR", X_OCTET, X_OCTET, 0, TPESVCERR};
+	return do_tpcall(&args);
+}
+
+// telling service to call tpreturn outside service routine should have no effect
+static int t4() {
+	thr_arg_t args = {1, "T4", "tpreturn outside service routing", "BAR", X_OCTET, X_OCTET, 0, 0};
+	return do_tpcall(&args);
+}
+
+static int t5() {
+	thr_arg_t args = {1, "T5", "tpreturn TPFAIL", "BAR", X_OCTET, X_OCTET, 0, TPESVCFAIL};
+	return do_tpcall(&args);
+}
+
+int run_client(int argc, char **argv) {
+	int tx = 0;
+	int res = 0;
+	int bug = 217;
+
+	if (argc > 1)
+		bug = atoi(argv[1]);
+
+	userlogc((char*) "starting test %d", bug);
+
+	if (tx && (tx_open() != TX_OK || tx_begin() != TX_OK))
+		userlogc((char*) "ERROR - Could not open or begin transaction: ");
+	else {
+		switch (bug) {
+		case 211:	res = bug211(); break;
+		case 2120:	res = bug212a(); break;
+		case 2121:	res = bug212b(); break;
+		case 213:	res = bug213(); break;
+		case 214:	res = bug214(); break;
+		case 215:	res = bug215(); break;
+		case 2160:	res = bug216a(); break;
+		case 2161:	res = bug216b(); break;
+
+		case 217:	res = bug217(); break;
+		case 9001:	res = t9001(); break;
+		case 0:		res = t0(); break;
+		case 1:		res = t1(); break;
+		case 2:		res = t2(); break;
+		case 3:		res = t3(); break;
+		case 4:		res = t4(); break;
+		case 5:		res = t5(); break;
+		default: break;
+		}
+
+		if (tx && (tx_commit() != TX_OK || tx_close() != TX_OK))
+			userlogc((char*) "ERROR - Could not commit or close transaction: ");
+	}
+
+	userlogc((char*) "test %d %s with code %d", bug, (res == 0 ? "passed" : "failed"), res);
+
+	return res;
+}
+#endif
+
+void BAR (TPSVCINFO *);
+void TestTPCall (TPSVCINFO *);
+
+void BAR(TPSVCINFO * svcinfo) {
+	char* buffer;
+	int sendlen = 15;
+	long rflag = 0L;
+	int rval = TPSUCCESS;
+
+	userlogc((char*) "bar called  - svc=%s data=%s len=%d flags=%d rcode=%d tperrno=%d",
+		svcinfo->name, svcinfo->data, svcinfo->len, svcinfo->flags, 99, tperrno);
+
+	if (strcmp(svcinfo->data, "T1") == 0)
+		rflag = TPEBLOCK;
+	else if (strcmp(svcinfo->data, "T2") == 0)
+		tpfree(svcinfo->data);
+	else if (strcmp(svcinfo->data, "T5") == 0)
+		rval = TPFAIL;
+
+	buffer = tpalloc((char *) "X_OCTET", 0, sendlen);
+	strcpy(buffer, "BAR SAYS HELLO");
+
+	if (strcmp(svcinfo->data, "T3") != 0)
+		tpreturn(rval, 99, buffer, sendlen, rflag);
+
+	if (strcmp(svcinfo->data, "T4") == 0)
+		tpreturn(TPFAIL, 99, buffer, sendlen, rflag);
+
+	if (tperrno)
+		userlogc((char*) "bar returned: tperrno=%d", tperrno);
+}
+
+void TestTPCall(TPSVCINFO * svcinfo) {
+	BAR(svcinfo);
+}
+
+int run_server(int argc, char **argv) {
+	int exit_status = serverinit(argc, argv);
+
+	if (exit_status != -1) {
+		tpadvertise((char *) "BAR", BAR);
+		tpadvertise((char *) "TestTPCall", TestTPCall);
+        //SERVICE_ADVERTISEMENTS
+		exit_status = serverrun();
+	} else {
+		userlogc((char*) "main Unexpected exception in serverrun()");
+	}
+	serverdone();
+	return exit_status;
+}
+
+int main(int argc, char **argv) {
+	int i;
+
+    for (i = 0; i < argc; i++) {
+        if (strcmp(argv[i], "-i") == 0)
+            return run_server(argc, argv);
+    }
+
+    return run_client(argc, argv);
+}
---
+++ integration-tests/src/test/java/org/jboss/blacktie/jatmibroker/xatmi/CSTest.java
@@ -0,0 +1,189 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2009, Red Hat, Inc., and others contributors as indicated
+ * by the @authors tag. All rights reserved.
+ * See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU Lesser General Public License, v. 2.1.
+ * This program is distributed in the hope that it will be useful, but WITHOUT A
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License,
+ * v.2.1 along with this distribution; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ */
+package org.jboss.blacktie.jatmibroker.xatmi;
+
+import java.io.*;
+import junit.framework.TestCase;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+public class CSTest extends TestCase
+{
+	private static final Logger log = LogManager.getLogger(CSTest.class);
+	private static boolean DISABLED = true;	// disable tests until the C program is built correctly
+
+	// Avoid having to start up and tear down a server for each test
+	// WARNING TODO NTESTS must match the actual number of tests
+	// Better would be to define
+	// public static Test suite() and have the final test destroy the server
+	static final int NTESTS = 16;	// Number of tests 
+	private static int testCnt = 0;
+
+	private static TestProcess server;
+	private TestProcess client;
+	private String CS_EXE;
+	private String REPORT_DIR;
+
+	public void tearDown() {
+		if (DISABLED) return;
+		log.info("tearDown " + (testCnt + 1) + " out of " + NTESTS);
+		if (++testCnt == NTESTS) {
+			try {
+				System.out.println("destroying server");
+				server.getProcess().destroy();
+			} catch(Throwable e) {
+			}
+		}
+	}
+
+	public void setUp() {
+		if (DISABLED) return;
+		REPORT_DIR = System.getProperty("TEST_REPORTS_DIR", ".");
+		CS_EXE = System.getProperty("CLIENT_SERVER_EXE", "./cs");
+
+		if (server == null) {
+			log.info("CS Tests: SETUP REPORT_DIR=" + REPORT_DIR + " CLIENT_SERVER_EXE=" + CS_EXE);
+
+			try {
+				server = startServer();
+				Thread.sleep(3000);
+			} catch (IOException e) {
+				System.err.printf("Server io exception: " + e);
+			} catch (InterruptedException e) {
+				System.err.printf("Server interrupted: " + e);
+			}
+		}
+	}
+
+	public void test_211() { runTest("211"); }
+	public void test_2120() { runTest("2120"); }
+	public void test_2121() { runTest("2121"); }
+	public void test_213() { runTest("213"); }
+	public void test_214() { runTest("214"); }
+	public void test_215() { runTest("215"); }
+	public void test_2160() { runTest("2160"); }
+	public void test_2161() { runTest("2161"); }
+	public void test_217() { runTest("217"); }
+	public void test_9001() { runTest("9001"); }
+	public void test_0() { runTest("0"); }
+	public void test_1() { runTest("1"); }
+	public void test_2() { runTest("2"); }
+	public void test_3() { runTest("3"); }
+	public void test_4() { runTest("4"); }
+	public void test_5() { runTest("5"); }
+
+	void runTest(String name) {
+		if (DISABLED) return;
+		try {
+			log.debug("waiting for test " + name);
+			TestProcess client = startClient(name, true);
+			int res = client.exitValue();
+
+			log.info("test " + name + (res == 0 ? " passed " : " failed ") + res);
+			assertTrue(res == 0);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		} catch (InterruptedException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	TestProcess startClient(String testname, boolean waitFor) throws IOException, InterruptedException {
+		FileOutputStream ostream = new FileOutputStream(REPORT_DIR + "/test-" + testname + "-out.txt");
+		FileOutputStream estream = new FileOutputStream(REPORT_DIR + "/test-" + testname + "-err.txt");
+		TestProcess client = new TestProcess(ostream, estream, "client: ", CS_EXE + " " + testname);
+		Thread thread = new Thread(client);
+
+		thread.start();
+		if (waitFor) {
+			log.debug("startClient: waiting to join with client thread ...");
+			thread.join();
+			log.debug("startClient: joined - waiting for client process to exit");
+			client.getProcess().waitFor();
+			log.debug("startClient: done - closing streams");
+			ostream.close();
+			estream.close();
+		} else {
+		}
+
+		return client;
+	}
+
+	TestProcess startServer() throws IOException {
+		FileOutputStream ostream = new FileOutputStream(REPORT_DIR + "/server-out.txt");
+		FileOutputStream estream = new FileOutputStream(REPORT_DIR + "/server-err.txt");
+		TestProcess server = new TestProcess(ostream, estream, "server: ", CS_EXE + " -c linux -i 1");
+		Thread thread = new Thread(server);
+		thread.start();
+
+		return server;
+	}
+
+	public static void main(String args[]) throws IOException {
+		new CSTest().runTests();
+	}
+
+	void runTests() {
+		setUp();
+
+//		test_211(); test_2120(); test_2121(); test_213(); test_214(); test_215();
+//		test_2160(); test_2161(); test_217(); test_9001();
+//		test_0(); test_1(); test_2(); test_3(); test_4(); test_5(); 
+		System.out.println("destroying server");
+		server.getProcess().destroy();
+	}
+
+	class TestProcess implements Runnable {
+		private String prefix;
+		private String command;
+		private Process proc;
+		private FileOutputStream ostream;
+		private FileOutputStream estream;
+
+		TestProcess(FileOutputStream ostream, FileOutputStream estream, String prefix, String command) {
+			this.ostream = ostream;
+			this.estream = estream;
+			this.prefix = prefix;
+			this.command = command;
+		}
+
+		Process getProcess() { return proc; }
+		int exitValue() { return proc.exitValue(); }
+		FileOutputStream getOStream() { return ostream; }
+
+		public void run() {
+			try {
+				Runtime rt = Runtime.getRuntime();
+				proc = rt.exec(command);
+
+				InputStream is = proc.getInputStream();
+				InputStream es = proc.getErrorStream();
+				byte[] buf = new byte[1024];
+				int len;
+
+				while ((len = is.read(buf)) > 0)
+					ostream.write(buf, 0, len);
+
+				while ((len = es.read(buf)) > 0)
+					estream.write(buf, 0, len);
+			} catch (IOException e) {
+				log.error("Error execing command: " + e);
+			}
+		}
+	}
+}
