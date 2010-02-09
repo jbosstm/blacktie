f15da2f7c62269d5e55e8e426687547f842f669f
Tom Jenkinson <tom.jenkinson@redhat.com>
2010-02-09 16:05:53 +0000

    BLACKTIE-274 updated to return -1
    git-svn-id: http://anonsvn.jboss.org/repos/blacktie/trunk@1706 aa7494b7-e75f-0410-880c-f61b6724bf28

--- scripts/test/bin.sh
+++ scripts/test/bin.sh
@@ -15,7 +15,7 @@ sed -i "s=REPLACE_WITH_INSTALL_LOCATION=$VAR=g" setenv.sh
 cd $BLACKTIE_BIN_DIR/examples/xatmi/fooapp
 $BLACKTIE_BIN_DIR/bin/generate_server.sh -Dservice.names=BAR -Dserver.includes=BarService.c
 if [ "$?" != "0" ]; then
-	exit $?
+	exit -1
 fi
 ./server -c linux -i 1&
 
@@ -23,14 +23,14 @@ fi
 $BLACKTIE_BIN_DIR/bin/generate_client.sh -Dclient.includes=client.c
 ./client
 if [ "$?" != "0" ]; then
-	exit $?
+	exit -1
 fi
 
 # RUN THE JAVA CLIENT
 cd $BLACKTIE_BIN_DIR/examples/jab
 echo hello | mvn test
 if [ "$?" != "0" ]; then
-	exit $?
+	exit -1
 fi
 
 # RUN THE ADMIN JMX CLIENT
@@ -40,6 +40,9 @@ echo '0
 0
 0
 1' | mvn test
+if [ "$?" != "0" ]; then
+	exit -1
+fi
 
 # PICK UP THE CLOSING SERVER
 fg
@@ -62,5 +65,5 @@ fg
 cd $BLACKTIE_BIN_DIR/examples/mdb
 mvn install
 if [ "$?" != "0" ]; then
-	exit $?
+	exit -1
 fi
