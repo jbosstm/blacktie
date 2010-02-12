c6b1425512f25dcbb813928dcba96779be1ce01c
Tom Jenkinson <tom.jenkinson@redhat.com>
2010-02-12 10:12:42 +0000

    BLACKTIE-262 updated to allow security example to run with default JBAS
    git-svn-id: http://anonsvn.jboss.org/repos/blacktie/trunk@1720 aa7494b7-e75f-0410-880c-f61b6724bf28

--- scripts/test/bin.sh
+++ scripts/test/bin.sh
@@ -1,5 +1,8 @@
-cd ../../
+# ALLOW JOBS TO BE BACKGROUNDED
 set -m
+
+# SET UP THE DISTRIBUTION
+cd ../../
 rm -rf tmp
 mkdir tmp
 cp blacktie/target/blacktie-*-bin.tar.gz tmp
@@ -9,9 +12,10 @@ cd blacktie*/
 export VAR=`pwd`
 sed -i "s=REPLACE_WITH_INSTALL_LOCATION=$VAR=g" setenv.sh
 
+# MAKE SURE THE ENVIRONMENT VARIABLES ARE SET
 . setenv.sh
 
-# RUN THE SERVER
+# RUN THE FOOAPP SERVER
 cd $BLACKTIE_BIN_DIR/examples/xatmi/fooapp
 $BLACKTIE_BIN_DIR/bin/generate_server.sh -Dservice.names=BAR -Dserver.includes=BarService.c
 if [ "$?" != "0" ]; then
@@ -43,11 +47,10 @@ echo '0
 if [ "$?" != "0" ]; then
 	exit -1
 fi
-
 # PICK UP THE CLOSING SERVER
 fg
 
-# RUN THE SERVER AGAIN
+# RUN THE FOOAPP SERVER AGAIN
 cd $BLACKTIE_BIN_DIR/examples/xatmi/fooapp
 ./server -c linux -i 1&
 
@@ -59,6 +62,7 @@ echo '0
 0
 0
 1' | ./client
+# PICK UP THE CLOSING SERVER
 fg
 
 # RUN THE MDB EXAMPLE
@@ -67,3 +71,44 @@ mvn install
 if [ "$?" != "0" ]; then
 	exit -1
 fi
+
+# RUN THE SECURE SERVER
+cd $BLACKTIE_BIN_DIR/examples/security
+$BLACKTIE_BIN_DIR/bin/generate_server.sh -Dservice.names=SECURE -Dserver.includes=BarService.c
+if [ "$?" != "0" ]; then
+	exit -1
+fi
+export BLACKTIE_CONFIGURATION_DIR=serv
+./server -c linux -i 1 secure&
+unset BLACKTIE_CONFIGURATION_DIR
+
+# RUN THE "guest" USER CLIENT
+$BLACKTIE_BIN_DIR/bin/generate_client.sh -Dclient.includes=client.c
+export BLACKTIE_CONFIGURATION_DIR=guest
+./client
+# This test is expected to fail so make sure the exit status was not 0
+if [ "$?" == "0" ]; then
+	exit -1
+fi
+unset BLACKTIE_CONFIGURATION_DIR
+
+# RUN THE "dynsub" USER CLIENT
+export BLACKTIE_CONFIGURATION_DIR=dynsub
+./client
+if [ "$?" != "0" ]; then
+	exit -1
+fi
+unset BLACKTIE_CONFIGURATION_DIR
+
+# SHUTDOWN THE SERVER RUNNING THE XATMI ADMIN CLIENT
+cd $BLACKTIE_BIN_DIR/examples/admin/xatmi
+$BLACKTIE_BIN_DIR/bin/generate_client.sh -Dclient.includes=client.c
+unset BLACKTIE_CONFIGURATION_DIR
+echo '0
+0
+0
+0
+1' | ./client
+# PICK UP THE CLOSING SERVER
+fg
+
