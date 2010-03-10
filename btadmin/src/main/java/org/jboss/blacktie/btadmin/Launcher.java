146b93645e19adba8d4b328ae909f3eac50a0dea
Tom Jenkinson <tom.jenkinson@redhat.com>
2010-03-10 12:53:31 +0000

    added the launcher
    git-svn-id: http://anonsvn.jboss.org/repos/blacktie/trunk@1789 aa7494b7-e75f-0410-880c-f61b6724bf28

---
+++ btadmin/src/main/java/org/jboss/blacktie/btadmin/Launcher.java
@@ -0,0 +1,13 @@
+package org.jboss.blacktie.btadmin;
+
+/**
+ * Launcher for the btadmin tool.
+ * 
+ * @author tomjenkinson
+ */
+public class Launcher {
+
+	public static void main(String[] args) {
+
+	}
+}
