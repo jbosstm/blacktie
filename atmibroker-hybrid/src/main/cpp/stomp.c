2f889dde880e19b71c0d110b1a26b68729a6c4f2
Tom Jenkinson <tom.jenkinson@redhat.com>
2010-03-11 11:21:00 +0000

    updated to work with c even better ;)
    git-svn-id: http://anonsvn.jboss.org/repos/blacktie/trunk@1807 aa7494b7-e75f-0410-880c-f61b6724bf28

--- atmibroker-hybrid/src/main/cpp/stomp.c
+++ atmibroker-hybrid/src/main/cpp/stomp.c
@@ -461,8 +461,8 @@ apr_status_t stomp_read(stomp_connection *connection, stomp_frame **frame, apr_p
 		  char* content_length = apr_hash_get(f->headers, "content-length", APR_HASH_KEY_STRING);
 		  if(content_length) {
 			  char endbuffer[2];
-			  userlogc_debug("Content-length detected");
 			  apr_size_t length = 2;
+			  userlogc_debug("Content-length detected");
 
 			  f->body_length = atoi(content_length);
 			  f->body = apr_pcalloc(pool, f->body_length);
