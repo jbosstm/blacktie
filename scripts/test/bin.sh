3c1f7f2cb321beb4a63199b17ec5416aae89c0da
Tom Jenkinson <tom.jenkinson@redhat.com>
2010-03-20 09:24:43 +0000

    updated to request the user to enter the hostname
    git-svn-id: http://anonsvn.jboss.org/repos/blacktie/trunk@1858 aa7494b7-e75f-0410-880c-f61b6724bf28

--- scripts/test/bin.sh
+++ scripts/test/bin.sh
@@ -7,9 +7,11 @@ tar xfz blacktie-*-bin.tar.gz
 cd blacktie*/
 
 # MAKE SURE THE ENVIRONMENT VARIABLES ARE SET
+export HOSTNAME_TO_USE=`hostname`
 export VAR=`pwd`
 sed -i "s=REPLACE_WITH_INSTALL_LOCATION=$VAR=g" setenv.sh
 for i in `find . -name Environment.xml`; do sed -i "s=REPLACE_WITH_INSTALL_LOCATION=$VAR=g" $i; done
+for i in `find . -name Environment.xml`; do sed -i "s=REPLACE_WITH_HOSTNAME=$HOSTNAME_TO_USE=g" $i; done
 . setenv.sh
 
 ./run_all_samples.sh
