eabb0638a98e558b76c9ce93530fe6c3af1a8ad6
Tom Jenkinson <tom.jenkinson@redhat.com>
2010-03-27 12:54:51 +0000

    Leave the shutdown for 15 seconds
    git-svn-id: http://anonsvn.jboss.org/repos/blacktie/trunk@1917 aa7494b7-e75f-0410-880c-f61b6724bf28

--- btadmin/src/main/java/org/jboss/blacktie/btadmin/commands/Shutdown.java
+++ btadmin/src/main/java/org/jboss/blacktie/btadmin/commands/Shutdown.java
@@ -135,7 +135,7 @@ public class Shutdown implements Command {
 
 			// TODO SHUTDOWN SHOULD RETURN WHEN THERE ARE NO MORE CONSUMERS?
 			try {
-				Thread.currentThread().sleep(10000);
+				Thread.currentThread().sleep(15000);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
