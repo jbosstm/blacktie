2d5efa77ef386954491094e190e7ffbcb1ac29ac
Tom Jenkinson <tom.jenkinson@redhat.com>
2010-03-30 11:46:26 +0000

    updated to replace more localhosts with the correct port name
    git-svn-id: http://anonsvn.jboss.org/repos/blacktie/trunk@1930 aa7494b7-e75f-0410-880c-f61b6724bf28

--- scripts/test/bin.sh
+++ scripts/test/bin.sh
@@ -10,6 +10,7 @@ export VAR=`pwd`
 sed -i "s=REPLACE_WITH_INSTALL_LOCATION=$VAR=g" setenv.sh
 for i in `find . -name btconfig.xml`; do sed -i "s=REPLACE_WITH_INSTALL_LOCATION=$VAR=g" $i; done
 for i in `find . -name btconfig.xml`; do sed -i "s=REPLACE_WITH_HOSTNAME=$HOSTNAME_TO_USE=g" $i; done
+for i in `find . -name pom.xml`; do sed -i "s=REPLACE_WITH_HOSTNAME=$HOSTNAME_TO_USE=g" $i; done
 . setenv.sh
 
 ./run_all_samples.sh
