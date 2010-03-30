348a2c1c64a96966bf23da2c0c8af66f458c4a9c
Tom Jenkinson <tom.jenkinson@redhat.com>
2010-03-30 13:25:02 +0000

    replace the orb host with the one used by jboss
    git-svn-id: http://anonsvn.jboss.org/repos/blacktie/trunk@1933 aa7494b7-e75f-0410-880c-f61b6724bf28

--- scripts/test/bin.sh
+++ scripts/test/bin.sh
@@ -11,6 +11,7 @@ sed -i "s=REPLACE_WITH_INSTALL_LOCATION=$VAR=g" setenv.sh
 for i in `find . -name btconfig.xml`; do sed -i "s=REPLACE_WITH_INSTALL_LOCATION=$VAR=g" $i; done
 for i in `find . -name btconfig.xml`; do sed -i "s=REPLACE_WITH_HOSTNAME=$HOSTNAME_TO_USE=g" $i; done
 for i in `find . -name pom.xml`; do sed -i "s=REPLACE_WITH_HOSTNAME=$HOSTNAME_TO_USE=g" $i; done
+for i in `find . -name btconfig.xml`; do sed -i "s=$HOSTNAME_TO_USE:3528=$ORB_HOST:3528=g" $i; done
 . setenv.sh
 
 ./run_all_samples.sh
