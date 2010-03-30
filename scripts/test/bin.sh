cfb8efac1f862513fb7de5e9d0a5d12af444403b
Tom Jenkinson <tom.jenkinson@redhat.com>
2010-03-30 14:44:00 +0000

    updated to change the port for jnp too
    git-svn-id: http://anonsvn.jboss.org/repos/blacktie/trunk@1935 aa7494b7-e75f-0410-880c-f61b6724bf28

--- scripts/test/bin.sh
+++ scripts/test/bin.sh
@@ -11,7 +11,8 @@ sed -i "s=REPLACE_WITH_INSTALL_LOCATION=$VAR=g" setenv.sh
 for i in `find . -name btconfig.xml`; do sed -i "s=REPLACE_WITH_INSTALL_LOCATION=$VAR=g" $i; done
 for i in `find . -name btconfig.xml`; do sed -i "s=REPLACE_WITH_HOSTNAME=$HOSTNAME_TO_USE=g" $i; done
 for i in `find . -name pom.xml`; do sed -i "s=REPLACE_WITH_HOSTNAME=$HOSTNAME_TO_USE=g" $i; done
-for i in `find . -name btconfig.xml`; do sed -i "s=$HOSTNAME_TO_USE:3528=$ORB_HOST:3528=g" $i; done
+for i in `find . -name btconfig.xml`; do sed -i "s=$HOSTNAME_TO_USE:3528=$JBOSSAS_IP_ADDR:3528=g" $i; done
+for i in `find . -name btconfig.xml`; do sed -i "s=$HOSTNAME_TO_USE:1099=$JBOSSAS_IP_ADDR:1099=g" $i; done
 . setenv.sh
 
 ./run_all_samples.sh
