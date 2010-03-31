999b75553a2373d0b02737fe830cb1656dea849b
Tom Jenkinson <tom.jenkinson@redhat.com>
2010-03-31 13:48:26 +0000

    updated to use the JBOSSAS_IP_ADDR
    git-svn-id: http://anonsvn.jboss.org/repos/blacktie/trunk@1955 aa7494b7-e75f-0410-880c-f61b6724bf28

--- scripts/test/bin.sh
+++ scripts/test/bin.sh
@@ -10,7 +10,7 @@ export VAR=`pwd`
 sed -i "s=REPLACE_WITH_INSTALL_LOCATION=$VAR=g" setenv.sh
 for i in `find . -name btconfig.xml`; do sed -i "s=REPLACE_WITH_INSTALL_LOCATION=$VAR=g" $i; done
 for i in `find . -name btconfig.xml`; do sed -i "s=REPLACE_WITH_HOSTNAME=$HOSTNAME_TO_USE=g" $i; done
-for i in `find . -name pom.xml`; do sed -i "s=REPLACE_WITH_HOSTNAME=$HOSTNAME_TO_USE=g" $i; done
+for i in `find . -name pom.xml`; do sed -i "s=REPLACE_WITH_HOSTNAME=$JBOSSAS_IP_ADDR=g" $i; done
 for i in `find . -name btconfig.xml`; do sed -i "s=$HOSTNAME_TO_USE:3528=$JBOSSAS_IP_ADDR:3528=g" $i; done
 for i in `find . -name btconfig.xml`; do sed -i "s=$HOSTNAME_TO_USE:109=$JBOSSAS_IP_ADDR:109=g" $i; done
 . setenv.sh
