b109a9ba0a9b8f5eb5c0cffbb3ce1b2eb46b8c81
Tom Jenkinson <tom.jenkinson@redhat.com>
2010-06-11 10:01:34 +0000

    updated to CR1
    git-svn-id: http://anonsvn.jboss.org/repos/blacktie/trunk@2209 aa7494b7-e75f-0410-880c-f61b6724bf28

--- atmibroker-xatmi/src/test/cpp/TestAdmin.cxx
+++ atmibroker-xatmi/src/test/cpp/TestAdmin.cxx
@@ -56,7 +56,7 @@ void TestAdmin::testVersion() {
 	cd = callADMIN((char*)"version", '1', 0, &ver);
 	BT_ASSERT(cd == 0);
 	userlogc((char*)"version is %s", ver);
-	BT_ASSERT(strcmp(ver, "2.0.0.M4") == 0);
+	BT_ASSERT(strcmp(ver, "2.0.0.CR1") == 0);
 	free(ver);
 }
 
