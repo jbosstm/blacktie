680d22f8a33580fc3aa67b77a74f85e3a772f574
Tom Jenkinson <tom.jenkinson@redhat.com>
2010-06-28 11:22:38 +0000

    readded the test
    git-svn-id: http://anonsvn.jboss.org/repos/blacktie/trunk@2222 aa7494b7-e75f-0410-880c-f61b6724bf28

--- atmibroker-xatmi/src/test/include/TestExternManageDestination.h
+++ atmibroker-xatmi/src/test/include/TestExternManageDestination.h
@@ -26,6 +26,7 @@
 class TestExternManageDestination : public BaseServerTest {
 	CPPUNIT_TEST_SUITE( TestExternManageDestination);
 	CPPUNIT_TEST( test_tpcall_with_service);
+	CPPUNIT_TEST( test_tpcall_without_service);
 	CPPUNIT_TEST_SUITE_END();
 public:
 	void test_tpcall_with_service();
