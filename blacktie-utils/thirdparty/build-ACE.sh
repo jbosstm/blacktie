cdd1e0650ed36276c4fab35e44922e9cfcdb9e89
Tom Jenkinson <tom.jenkinson@redhat.com>
2010-07-21 14:13:06 +0000

    added so that building ace is easier
    git-svn-id: http://anonsvn.jboss.org/repos/blacktie/trunk@2245 aa7494b7-e75f-0410-880c-f61b6724bf28

---
+++ blacktie-utils/thirdparty/build-ACE.sh
@@ -0,0 +1,14 @@
+echo "Making ACE+TAO-$ACE_VER"
+rm -rf ACE+TAO-$ACE_VER
+mkdir ACE+TAO-$ACE_VER
+tar xfz ACE+TAO-$ACE_VER.tar.gz -C ACE+TAO-$ACE_VER
+export ACE_ROOT=/home/hudson/blacktie/utils/ACE+TAO-$ACE_VER/ACE_wrappers
+export TAO_ROOT=/home/hudson/blacktie/utils/ACE+TAO-$ACE_VER/ACE_wrappers/TAO
+export LD_LIBRARY_PATH=$ACE_ROOT/lib
+cd $ACE_ROOT
+echo "#include \"ace/config-linux.h\"" > $ACE_ROOT/ace/config.h
+echo "include \$(ACE_ROOT)/include/makeinclude/platform_linux.GNU" > $ACE_ROOT/include/makeinclude/platform_macros.GNU
+for i in ace apps/gperf/src ACEXML $TAO_ROOT/TAO_IDL $TAO_ROOT/tao $TAO_ROOT/orbsvcs/ImplRepo_Service $TAO_ROOT/orbsvcs/orbsvcs
+do
+(cd $i && make)
+done
