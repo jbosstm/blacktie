5a296b95967ce1e4182697984e5d8397d10af862
Tom Jenkinson <tom.jenkinson@redhat.com>
2010-07-21 12:30:26 +0000

    updated with correct path
    git-svn-id: http://anonsvn.jboss.org/repos/blacktie/trunk@2243 aa7494b7-e75f-0410-880c-f61b6724bf28

--- blacktie-utils/thirdparty/deploy.sh
+++ blacktie-utils/thirdparty/deploy.sh
@@ -2,11 +2,11 @@ export bpa=centos55x32
 
 for i in ace-5.7.6 cppunit-1.12 expat-2.0.1 xercesc-3.0.1
 do
-mvn deploy:deploy-file -Durl=https://repository.jboss.org/nexus/service/local/staging/deploy/maven2/ -DrepositoryId=jboss-releases -Dfile=`pwd`/build/$i-$bpa.zip -DpomFile=poms/$1.pom -Dpackaging=zip -Dclassifier=$bpa
+mvn deploy:deploy-file -Durl=https://repository.jboss.org/nexus/service/local/staging/deploy/maven2/ -DrepositoryId=jboss-releases -Dfile=`pwd`/build/lib/$i-$bpa.zip -DpomFile=`pwd`/poms/$i.pom -Dpackaging=zip -Dclassifier=$bpa
 done
 
 
 for i in apr-1-1.3.3 log4cxx-902683
 do
-mvn deploy:deploy-file -Durl=https://repository.jboss.org/nexus/service/local/staging/deploy/maven2/ -DrepositoryId=jboss-releases -Dfile=`pwd`/build/$i-$bpa.jar -DpomFile=poms/$1.pom -Dpackaging=jar -Dclassifier=$bpa
+mvn deploy:deploy-file -Durl=https://repository.jboss.org/nexus/service/local/staging/deploy/maven2/ -DrepositoryId=jboss-releases -Dfile=`pwd`/build/lib/$i-$bpa.jar -DpomFile=`pwd`/poms/$i.pom -Dpackaging=jar -Dclassifier=$bpa
 done
