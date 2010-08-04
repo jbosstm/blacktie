85896da59ebe40b37fdad7e602beb149ba2a6e97
Tom Jenkinson <tom.jenkinson@redhat.com>
2010-08-04 08:33:11 +0000

    BLACKTIE-320 updated with unit test
    git-svn-id: http://anonsvn.jboss.org/repos/blacktie/trunk@2303 aa7494b7-e75f-0410-880c-f61b6724bf28

--- atmibroker-xatmi/src/test/cpp/TestExternManageDestination.cxx
+++ atmibroker-xatmi/src/test/cpp/TestExternManageDestination.cxx
@@ -19,8 +19,13 @@
 #include "TestExternManageDestination.h"
 #include "BaseServerTest.h"
 
+#include "AtmiBrokerServerControl.h"
+
 #include "xatmi.h"
 #include "malloc.h"
+
+#include "ace/OS_NS_stdlib.h"
+#include "ace/OS_NS_stdio.h"
 
 #if defined(__cplusplus)
 extern "C" {
@@ -30,28 +35,29 @@ extern void test_extern_service(TPSVCINFO *svcinfo);
 }
 #endif
 
+static int msgCnt;
+
 void TestExternManageDestination::setUp() {
 	userlogc((char*) "TestExternManageDestination::setUp");
 	unadvertised = false;
 
-	// Set up server
-	BaseServerTest::setUp();
+	ACE_OS::putenv("BLACKTIE_SERVER_ID=1");
 
-	int toCheck = tpadvertise((char*) "TestOne", test_extern_service);
-	userlogc((char*) "TestExternManageDestination %d %d", toCheck, tperrno);
-	BT_ASSERT(tperrno == 0);
-	BT_ASSERT(toCheck != -1);
+	// Set up server
+	BaseTest::setUp();
 }
 
 void TestExternManageDestination::tearDown() {
 	userlogc((char*) "TestTPACall::tearDown");
 
-	if(unadvertised == false) {
+	if (unadvertised == false) {
 		int toCheck = tpunadvertise((char*) "TestOne");
 		userlogc((char*) "TestExternManageDestination %d %d", toCheck, tperrno);
 		BT_ASSERT(tperrno == 0);
 		BT_ASSERT(toCheck != -1);
 	}
+
+	ACE_OS::putenv("BLACKTIE_SERVER_ID=");
 
 	// Clean up server
 	BaseServerTest::tearDown();
@@ -60,15 +66,22 @@ void TestExternManageDestination::tearDown() {
 void TestExternManageDestination::test_tpcall_with_service() {
 	userlogc((char*) "test_tpcall_with_service");
 
+	// Advertise the service
+	int toCheck = tpadvertise((char*) "TestOne", test_extern_service);
+	userlogc((char*) "TestExternManageDestination %d %d", toCheck, tperrno);
+	BT_ASSERT(tperrno == 0);
+	BT_ASSERT(toCheck != -1);
+
 	char msg[80];
 	char* buf = (char*) "test";
-	long  sendlen = strlen(buf) + 1;
+	long sendlen = strlen(buf) + 1;
 	char* sendbuf = tpalloc((char*) "X_OCTET", NULL, sendlen);
 	strcpy(sendbuf, buf);
 	char* recvbuf = tpalloc((char*) "X_OCTET", NULL, 1);
-	long  recvlen = 1;
+	long recvlen = 1;
 
-	int cd = ::tpcall((char*) "TestOne", (char *) sendbuf, sendlen, (char**)&recvbuf, &recvlen, 0);
+	int cd = ::tpcall((char*) "TestOne", (char *) sendbuf, sendlen,
+			(char**) &recvbuf, &recvlen, 0);
 
 	sprintf(msg, "%d %d (%s)", tperrno, cd, recvbuf);
 	BT_ASSERT_MESSAGE(msg, cd != -1);
@@ -83,15 +96,15 @@ void TestExternManageDestination::test_tpcall_without_service() {
 	userlogc((char*) "test_tpcall_without_service");
 
 	char* buf = (char*) "test";
-	long  sendlen = strlen(buf) + 1;
+	long sendlen = strlen(buf) + 1;
 	char* sendbuf = tpalloc((char*) "X_OCTET", NULL, sendlen);
 	strcpy(sendbuf, buf);
 	char* recvbuf = tpalloc((char*) "X_OCTET", NULL, 1);
-	long  recvlen = 1;
+	long recvlen = 1;
 
-	tpunadvertise((char*) "TestOne");
 	unadvertised = true;
-	int cd = ::tpcall((char*) "TestOne", (char *) sendbuf, sendlen, (char**)&recvbuf, &recvlen, 0);
+	int cd = ::tpcall((char*) "TestOne", (char *) sendbuf, sendlen,
+			(char**) &recvbuf, &recvlen, 0);
 	userlogc((char*) "test_tpcall_without_service %d %d", cd, tperrno);
 	BT_ASSERT(cd == -1);
 	/*
@@ -100,10 +113,61 @@ void TestExternManageDestination::test_tpcall_without_service() {
 	BT_ASSERT(tperrno == TPETIME);
 }
 
+void TestExternManageDestination::test_stored_messages() {
+	for (int i = 0; i < 10; i++) {
+		char msg[80];
+		sprintf(msg, (char*) "request %d", i);
+		long sendlen = strlen(msg) + 1;
+		char* sendbuf = tpalloc((char*) "X_OCTET", NULL, sendlen);
+		(void) strcpy(sendbuf, msg);
+		int cd = tpacall("TestOne", sendbuf, sendlen, TPNOREPLY);
+
+		if (cd != 0 || tperrno != 0)
+			userlogc((char*) "tpacall returned %d %d", cd, tperrno);
+		else
+			userlogc((char*) "sent %d:", i);
+
+		tpfree(sendbuf);
+	}
+
+	// Advertise the service
+	int toCheck = tpadvertise((char*) "TestOne", test_extern_service);
+	userlogc((char*) "TestExternManageDestination %d %d", toCheck, tperrno);
+	BT_ASSERT(tperrno == 0);
+	BT_ASSERT(toCheck != -1);
+
+	msgCnt = 5;
+	maxSleep = 10;
+	while (msgCnt > 0 && maxSleep-- > 0)
+		if (sleep(1) != 0)
+			break;
+
+	// Shutdown the server
+	serverdone();
+
+	// Advertise the service
+	toCheck = tpadvertise((char*) "TestOne", test_extern_service);
+	userlogc((char*) "TestExternManageDestination %d %d", toCheck, tperrno);
+	BT_ASSERT(tperrno == 0);
+	BT_ASSERT(toCheck != -1);
+
+	msgCnt = 5;
+	maxSleep = 10;
+	while (msgCnt > 0 && maxSleep-- > 0)
+		if (sleep(1) != 0)
+			break;
+}
+
 void test_extern_service(TPSVCINFO *svcinfo) {
 	userlogc((char*) "test_extern_service");
 	int len = 8;
 	char *toReturn = ::tpalloc((char*) "X_OCTET", NULL, len);
 	strcpy(toReturn, "testone");
 	tpreturn(TPSUCCESS, 0, toReturn, len, 0);
+}
+
+void qservice(TPSVCINFO *svcinfo) {
+	userlogc((char*) "svc: %s data: %s len: %d flags: %d", svcinfo->name,
+			svcinfo->data, svcinfo->len, svcinfo->flags);
+	msgCnt -= 1;
 }
