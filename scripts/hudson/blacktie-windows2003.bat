51be78094dd1ec4f429921993f8c310f2cf41559
Tom Jenkinson <tom.jenkinson@redhat.com>
2010-08-23 12:47:13 +0000

    Updated to set the MACHINE_ADDR correctly in a bat file
    git-svn-id: http://anonsvn.jboss.org/repos/blacktie/trunk@2417 aa7494b7-e75f-0410-880c-f61b6724bf28

--- scripts/hudson/blacktie-windows2003.bat
+++ scripts/hudson/blacktie-windows2003.bat
@@ -79,7 +79,7 @@ IF %ERRORLEVEL% NEQ 0 exit -1
 
 rem CREATE BLACKTIE DISTRIBUTION
 cd %WORKSPACE%\trunk\scripts\test
-for /f "delims=" %a in ('hostname') do @set MACHINE_ADDR=%a
+for /f "delims=" %%a in ('hostname') do @set MACHINE_ADDR=%%a
 call ant dist -DBT_HOME=C:\\\\hudson\\\\workspace\\\\blacktie-windows2003\\\\trunk\\\\dist\\\\ -DVERSION=blacktie-2.0.0.CR1 -DJBOSSAS_IP_ADDR=%JBOSSAS_IP_ADDR% -DMACHINE_ADDR=%MACHINE_ADDR%
 IF %ERRORLEVEL% NEQ 0 exit -1
 
