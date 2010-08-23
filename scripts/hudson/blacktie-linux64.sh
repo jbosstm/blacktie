e278fdfc0785d8a930072bad3dcdddb9410746ee
Tom Jenkinson <tom.jenkinson@redhat.com>
2010-08-23 10:05:37 +0000

    updated to use the workspace environment variable
    git-svn-id: http://anonsvn.jboss.org/repos/blacktie/trunk@2404 aa7494b7-e75f-0410-880c-f61b6724bf28

--- scripts/hudson/blacktie-linux64.sh
+++ scripts/hudson/blacktie-linux64.sh
@@ -3,7 +3,7 @@ if [ -n "${WORKSPACE+x}" ]; then
   echo WORKSPACE is set
 else
   echo WORKSPACE not set
-  cd $WORKSPACE
+  exit
 fi
 
 # SHUTDOWN ANY RUNNING JBOSS
@@ -25,45 +25,40 @@ wget http://albany/userContent/blacktie/jboss-5.1.0.GA.zip
 unzip jboss-5.1.0.GA.zip
 cd $WORKSPACE/jboss-5.1.0.GA/docs/examples/transactions
 ant jts
-cd ../../../server/all/conf
+cd $WORKSPACE/jboss-5.1.0.GA/server/all/conf
 cat jbossts-properties.xml | sed 's/CONFIGURATION_FILE/NAME_SERVICE/g' > jbossts-properties.xml.bak
 mv jbossts-properties.xml.bak jbossts-properties.xml
-cd ../../../
-pwd
-sed -i 's=Xmx128=Xmx768=g' bin/run.conf
-sed -i 's=Xmx512=Xmx768=g' bin/run.conf
-cd ../
+cd $WORKSPACE/jboss-5.1.0.GA/bin
+sed -i 's=Xmx128=Xmx768=g' run.conf
+sed -i 's=Xmx512=Xmx768=g' run.conf
 
 # INITIALIZE THE BLACKTIE JBOSS DEPENDENCIES
-cp trunk/jatmibroker-xatmi/src/test/resources/jatmibroker-xatmi-test-service.xml $WORKSPACE/jboss-5.1.0.GA/server/all/deploy
-cp trunk/blacktie-admin-services/src/test/resources/btconfig.xml $WORKSPACE/jboss-5.1.0.GA/server/all/conf
+cp $WORKSPACE/trunk/jatmibroker-xatmi/src/test/resources/jatmibroker-xatmi-test-service.xml $WORKSPACE/jboss-5.1.0.GA/server/all/deploy
+cp $WORKSPACE/trunk/blacktie-admin-services/src/test/resources/btconfig.xml $WORKSPACE/jboss-5.1.0.GA/server/all/conf
 
 # START JBOSS
 $WORKSPACE/jboss-5.1.0.GA/bin/run.sh -c all -b localhost&
 sleep 53
 
 # BUILD BLACKTIE CPP PLUGIN
-cd trunk/blacktie-utils/cpp-plugin
+cd $WORKSPACE/trunk/blacktie-utils/cpp-plugin
 mvn clean install
-cd ../../../
 
 # BUILD BLACKTIE
-cd trunk/blacktie
+cd $WORKSPACE/trunk/blacktie
 # THESE ARE SEPARATE SO WE DO NOT COPY THE OLD ARTIFACTS IF THE BUILD FAILS
 mvn clean
 mvn install -Dbpa=centos54x64
-cd ../jatmibroker-xatmi
+cd $WORKSPACE/trunk/jatmibroker-xatmi
 mvn site -DskipTests
-cd ../../
 
 # INITIALIZE THE BLACKTIE DISTRIBUTION
 BLACK_HOME=`pwd`
-cd trunk/scripts/test
+cd $WORKSPACE/trunk/scripts/test
 ant dist -DBT_HOME=${BLACK_HOME}/dist/ -DVERSION=blacktie-2.0.0.CR1 -DMACHINE_ADDR=`hostname`-DJBOSSAS_IP_ADDR=localhost -Dbpa=centos54x64
-cd ../../..
 
 # RUN ALL THE SAMPLES
-cd trunk/dist/blacktie-2.0.0.CR1/
+cd $WORKSPACE/trunk/dist/blacktie-2.0.0.CR1/
 . setenv.sh
 export ORACLE_HOME=/usr/lib/oracle/11.2/client64
 export ORACLE_LIB_DIR=/usr/lib/oracle/11.2/client64/lib
@@ -71,7 +66,6 @@ export ORACLE_INC_DIR=/usr/include/oracle/11.2/client64
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ORACLE_LIB_DIR
 export TNS_ADMIN=$WORKSPACE/instantclient_11_2/network/admin
 ./run_all_samples.sh tx
-cd ../../../
 
 # SHUTDOWN JBOSS (THIS SHOULD ALWAYS HAPPEN IDEALLY)
 echo foo | $WORKSPACE/jboss-5.1.0.GA/bin/shutdown.sh -S && cd .
