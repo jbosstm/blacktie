bc2ed6126507497b82cd52c1302e1916afcfbaf4
Tom Jenkinson <tom.jenkinson@redhat.com>
2010-11-26 10:09:58 +0000

    replace correct file
    git-svn-id: http://anonsvn.jboss.org/repos/blacktie/trunk@2538 aa7494b7-e75f-0410-880c-f61b6724bf28

--- scripts/hudson/blacktie-windows2003.bat
+++ scripts/hudson/blacktie-windows2003.bat
@@ -31,10 +31,11 @@ echo        ^<unzip src="./hornetq-2.1.2.Final.zip" dest="."/^> >> build.xml
 echo    ^</target^> >> build.xml
 echo    ^<target name="replaceJBoss"^> >> build.xml
 echo        ^<replaceregexp byline="true" file="jboss-5.1.0.GA/server/all-with-hornetq/conf/jbossts-properties.xml" match="CONFIGURATION_FILE" replace="NAME_SERVICE"  /^> >> build.xml
-echo        ^<replaceregexp byline="true" file="jboss-5.1.0.GA/server/all-with-hornetq/conf/jacorb.properties" match="localhost" replace="${JBOSSAS_IP_ADDR}"  /^> >> build.xml
+echo        ^<replaceregexp byline="true" file="jboss-5.1.0.GA/server/all-with-hornetq/conf/jacorb.properties" match="localhost" replace="${env.JBOSSAS_IP_ADDR}"  /^> >> build.xml
 echo    ^</target^> >> build.xml
+echo    ^<property environment="env"/^> >> build.xml
 echo	^<target name="replaceBlackTie"^> >> build.xml
-echo        ^<replaceregexp byline="true" file="trunk/blacktie-admin-services/src/main/resources/btconfig.xml" match="localhost" replace="${JBOSSAS_IP_ADDR}"  /^> >> build.xml
+echo        ^<replaceregexp byline="true" file="trunk/blacktie-admin-services/ear/src/main/application/btconfig.xml" match="localhost" replace="${env.JBOSSAS_IP_ADDR}"  /^> >> build.xml
 echo	^</target^> >> build.xml
 echo	^<target name="initializeBlackTieAdminSecurity"^> >> build.xml
 echo        ^<replaceregexp byline="true" file="jboss-5.1.0.GA/server/all-with-hornetq/deploy/hornetq.sar/hornetq-configuration.xml" match="&lt;/security-settings&gt;" replace="&lt;security-setting match=&quot;jms.queue.BTR_BTDomainAdmin&quot;&gt;         &lt;permission type=&quot;send&quot; roles=&quot;blacktie,guest&quot;/&gt;         &lt;permission type=&quot;consume&quot; roles=&quot;blacktie,guest&quot;/&gt;      &lt;/security-setting&gt;      &lt;security-setting match=&quot;jms.queue.BTR_BTStompAdmin&quot;&gt;         &lt;permission type=&quot;send&quot; roles=&quot;blacktie,guest&quot;/&gt;         &lt;permission type=&quot;consume&quot; roles=&quot;blacktie,guest&quot;/&gt;      &lt;/security-setting&gt;&lt;/security-settings&gt;"  /^> >> build.xml
