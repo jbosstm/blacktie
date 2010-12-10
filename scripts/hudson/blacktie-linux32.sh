d7c792e5c6e45f68e28c6a40d0d01388fdb814d8
Tom Jenkinson <tom.jenkinson@redhat.com>
2010-12-10 13:56:31 +0000

    shutdown processes in case of failure
    git-svn-id: http://anonsvn.jboss.org/repos/blacktie/trunk@2582 aa7494b7-e75f-0410-880c-f61b6724bf28

--- scripts/hudson/blacktie-linux32.sh
+++ scripts/hudson/blacktie-linux32.sh
@@ -73,6 +73,11 @@ sleep 53
 cd $WORKSPACE/trunk/blacktie-utils/cpp-plugin
 mvn clean install
 if [ "$?" != "0" ]; then
+	echo foo | $WORKSPACE/jboss-5.1.0.GA/bin/shutdown.sh -S && cd .
+	killall -9 testsuite
+	killall -9 server
+	killall -9 client
+	killall -9 cs
 	exit -1
 fi
 
@@ -81,16 +86,31 @@ cd $WORKSPACE/trunk/blacktie
 # THESE ARE SEPARATE SO WE DO NOT COPY THE OLD ARTIFACTS IF THE BUILD FAILS
 mvn clean
 if [ "$?" != "0" ]; then
+	echo foo | $WORKSPACE/jboss-5.1.0.GA/bin/shutdown.sh -S && cd .
+	killall -9 testsuite
+	killall -9 server
+	killall -9 client
+	killall -9 cs
 	exit -1
 fi
 mvn install -Dbpa=centos55x32 -Duse.valgrind=true
 if [ "$?" != "0" ]; then
+	echo foo | $WORKSPACE/jboss-5.1.0.GA/bin/shutdown.sh -S && cd .
+	killall -9 testsuite
+	killall -9 server
+	killall -9 client
+	killall -9 cs
 	exit -1
 fi
 # THIS IS TO RUN THE TESTS IN CODECOVERAGE
 cd $WORKSPACE/trunk/jatmibroker-xatmi
 mvn site
 if [ "$?" != "0" ]; then
+	echo foo | $WORKSPACE/jboss-5.1.0.GA/bin/shutdown.sh -S && cd .
+	killall -9 testsuite
+	killall -9 server
+	killall -9 client
+	killall -9 cs
 	exit -1
 fi
 
@@ -98,6 +118,11 @@ fi
 cd $WORKSPACE/trunk/scripts/test
 ant dist -DBT_HOME=$WORKSPACE/trunk/dist/ -DVERSION=blacktie-3.0.0.M1-SNAPSHOT -DMACHINE_ADDR=`hostname` -DJBOSSAS_IP_ADDR=localhost -Dbpa=centos55x32
 if [ "$?" != "0" ]; then
+	echo foo | $WORKSPACE/jboss-5.1.0.GA/bin/shutdown.sh -S && cd .
+	killall -9 testsuite
+	killall -9 server
+	killall -9 client
+	killall -9 cs
 	exit -1
 fi
 
@@ -105,6 +130,11 @@ fi
 cd $WORKSPACE/trunk/dist/blacktie-3.0.0.M1-SNAPSHOT/
 . setenv.sh
 if [ "$?" != "0" ]; then
+	echo foo | $WORKSPACE/jboss-5.1.0.GA/bin/shutdown.sh -S && cd .
+	killall -9 testsuite
+	killall -9 server
+	killall -9 client
+	killall -9 cs
 	exit -1
 fi
 export ORACLE_HOME=/usr/lib/oracle/11.2/client
@@ -122,6 +152,11 @@ sed -i 's?</security-settings>?      <security-setting match="jms.queue.BTR_SECU
 
 ./run_all_samples.sh tx
 if [ "$?" != "0" ]; then
+	echo foo | $WORKSPACE/jboss-5.1.0.GA/bin/shutdown.sh -S && cd .
+	killall -9 testsuite
+	killall -9 server
+	killall -9 client
+	killall -9 cs
 	exit -1
 fi
 
