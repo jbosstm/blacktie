88952d2e7d6241da3d4549bedee8ad79c472e452
Tom Jenkinson <tom.jenkinson@redhat.com>
2010-12-21 17:23:05 +0000

    BLACKTIE-339 updated to remove the -Wall for AIX
    git-svn-id: http://anonsvn.jboss.org/repos/blacktie/trunk@2596 aa7494b7-e75f-0410-880c-f61b6724bf28

--- codeGeneration/src/main/scripts/generate_client.bat
+++ codeGeneration/src/main/scripts/generate_client.bat
@@ -1,1 +1,1 @@
-java org.apache.tools.ant.launch.Launcher -f %BLACKTIE_HOME%/bin/generate-executables.xml -Dconfiguration.type=debug -Dbasedir=. -Dtoolchain.compiler=msvc client %*
+java org.apache.tools.ant.launch.Launcher -f %BLACKTIE_HOME%/bin/generate-executables.xml -Dconfiguration.type=debug -Dbasedir=. client %*
