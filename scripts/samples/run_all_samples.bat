ad8995c715a3f953c30efac8273431c14476c5e6
Tom Jenkinson <tom.jenkinson@redhat.com>
2011-03-25 16:33:51 +0000

    added an example to show the mapped names in action
    git-svn-id: http://anonsvn.jboss.org/repos/blacktie/trunk@2811 aa7494b7-e75f-0410-880c-f61b6724bf28

--- scripts/samples/run_all_samples.bat
+++ scripts/samples/run_all_samples.bat
@@ -207,6 +207,28 @@ call btadmin shutdown
 IF %ERRORLEVEL% NEQ 0 exit -1
 set BLACKTIE_CONFIGURATION=
 
+rem RUN MAPPED SERVICE NAMES EXAMPLE 
+cd %BLACKTIE_HOME%\examples\xatmi\mappedNames
+call generate_server -Dservice.names=ONE,TWO -Dserver.executable.file=hiprio  -Dserver.includes=BarService.c -Dserver.name=hiprio
+IF %ERRORLEVEL% NEQ 0 exit -1
+call generate_server -Dservice.names=THREE,FOUR -Dserver.executable.file=loprio  -Dserver.includes=BarService.c -Dserver.name=loprio
+IF %ERRORLEVEL% NEQ 0 exit -1
+set BLACKTIE_CONFIGURATION=win32
+call btadmin startup
+IF %ERRORLEVEL% NEQ 0 exit -1
+set BLACKTIE_CONFIGURATION=
+
+rem RUN THE C CLIENT
+call generate_client -Dclient.includes=client.c
+client
+IF %ERRORLEVEL% NEQ 0 exit -1
+
+rem SHUTDOWN THE SERVER RUNNING THE btadmin TOOL
+set BLACKTIE_CONFIGURATION=win32
+call btadmin shutdown
+IF %ERRORLEVEL% NEQ 0 exit -1
+set BLACKTIE_CONFIGURATION=
+
 rem RUN THE TXFOOAPP SERVER
 IF ["%1"] EQU ["tx"] (
 echo "Running txfooapp"
