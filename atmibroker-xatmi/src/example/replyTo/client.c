cf80555804468c953d584149e4d17458dd6c60ed
Tom Jenkinson <tom.jenkinson@redhat.com>
2011-03-29 15:33:59 +0000

    BLACKTIE-361 added an example to show how to do replyTo so a client can receive asynchronous notifications from a server
    git-svn-id: http://anonsvn.jboss.org/repos/blacktie/trunk@2823 aa7494b7-e75f-0410-880c-f61b6724bf28

---
+++ atmibroker-xatmi/src/example/replyTo/client.c
@@ -0,0 +1,76 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2008, Red Hat, Inc., and others contributors as indicated
+ * by the @authors tag. All rights reserved.
+ * See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU Lesser General Public License, v. 2.1.
+ * This program is distributed in the hope that it will be useful, but WITHOUT A
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License,
+ * v.2.1 along with this distribution; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ */
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "xatmi.h"
+
+#include "userlogc.h"
+
+#include "message.h"
+#include "AtmiBrokerServerControl.h"
+
+void message_handler(TPSVCINFO * svcinfo) {
+	userlogc((char*) "client handler called %s", svcinfo->data);
+}
+
+int main(int argc, char **argv) {
+    int tpstatus;
+	char *retbuf;
+	long retbufsize;
+    MESSAGE *message;
+
+    if (argc < 4) {
+        userlogc("Usage: %s <SERVER_NAME> <REPLY_TO> <MESSAGE>", argv[0]);
+        return -1;
+    }
+	
+    // THIS MUST BE DONE TO MAKE SURE THAT THE CLIENT CAN RECEIVE MESSAGES
+    setenv("BLACKTIE_SERVER", argv[1], 1);
+    setenv("BLACKTIE_SERVER_ID", "1", 1);
+    serverinit(0, NULL);
+
+    // CREATE A MESSAGE WITH A REPLY_TO FIELD SET
+	message = (MESSAGE*) tpalloc((char*) "X_COMMON", (char*) "message", 0);
+	strcpy(message->reply_to, argv[2]);
+	strcpy(message->data, argv[3]);
+
+    retbufsize = 15;
+    retbuf = (char *) tpalloc((char*) "X_OCTET", NULL, retbufsize);
+
+
+    userlogc("Please press return when you are ready to send a message");
+	getchar();
+
+	tpstatus = tpacall((char*) "FOO", (char*) message, 0, 0);
+	
+	if (tpstatus == -1 && tperrno == TPENOENT) {
+        userlogc("Service failed to operate");
+		tpfree((char*)message);
+		tpfree(retbuf);
+		return -1;
+	}
+
+    userlogc("Please press return after you have received a message");
+	getchar();
+
+	tpfree((char*)message);
+	tpfree(retbuf);
+	return 0;
+}
