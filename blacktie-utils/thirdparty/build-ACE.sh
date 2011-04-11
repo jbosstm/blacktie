062670d0885c8429296e9103179faff5f9674d0f
Tom Jenkinson <tom.jenkinson@redhat.com>
2011-04-11 14:19:18 +0000

    BLACKTIE-147 updated to handle ACE 6.0.1
    git-svn-id: http://anonsvn.jboss.org/repos/blacktie/trunk@2900 aa7494b7-e75f-0410-880c-f61b6724bf28

--- blacktie-utils/thirdparty/build-ACE.sh
+++ blacktie-utils/thirdparty/build-ACE.sh
@@ -1,16 +1,42 @@
+if [ "${ACE_VER}" == "" ]; then
+    echo ACE_VER not set
+    exit -1
+fi
+
+if [ "${bpa}" == "" ]; then
+    echo bpa not set
+    exit -1
+fi
+
 echo "Making ACE+TAO-$ACE_VER"
 rm -rf ACE+TAO-$ACE_VER
 mkdir ACE+TAO-$ACE_VER
-wget http://download.dre.vanderbilt.edu/previous_versions/ACE+TAO-$ACE_VER.tar.gz
-tar xfz ACE+TAO-$ACE_VER.tar.gz -C ACE+TAO-$ACE_VER
+wget http://download.dre.vanderbilt.edu/previous_versions/ACE+TAO-$ACE_VER.zip
+if [ "$?" != 0 ]; then
+    echo could not download
+    exit -1
+fi
+(cd ACE+TAO-$ACE_VER && unzip ../ACE+TAO-$ACE_VER.zip)
 export ACE_ROOT=`pwd`/ACE+TAO-$ACE_VER/ACE_wrappers
 export TAO_ROOT=`pwd`/ACE+TAO-$ACE_VER/ACE_wrappers/TAO
 export LD_LIBRARY_PATH=$ACE_ROOT/lib
-cd $ACE_ROOT
+
+dos2unix $ACE_ROOT/ace/bin/*.sh
 echo "#define ACE_INITIALIZE_MEMORY_BEFORE_USE 1
 #include \"ace/config-linux.h\"" > $ACE_ROOT/ace/config.h
+
 echo "include \$(ACE_ROOT)/include/makeinclude/platform_linux.GNU" > $ACE_ROOT/include/makeinclude/platform_macros.GNU
-for i in ace apps/gperf/src ACEXML $TAO_ROOT/TAO_IDL $TAO_ROOT/tao $TAO_ROOT/orbsvcs/ImplRepo_Service $TAO_ROOT/orbsvcs/orbsvcs
+
+for i in ace apps/gperf/src ACEXML $TAO_ROOT/TAO_IDL $TAO_ROOT/tao
 do
 (cd $i && make)
 done
+
+(cd  $TAO_ROOT/orbsvcs/orbsvcs && make CosNaming_Serv)
+
+
+ant -Dlinux=true ace
+
+if [ "$1" = "deploy" ]; then
+mvn deploy:deploy-file -Durl=https://repository.jboss.org/nexus/service/local/staging/deploy/maven2/ -DrepositoryId=jboss-releases -Dfile=`pwd`/build/lib/ace-$ACE_VER-$bpa.zip -DpomFile=`pwd`/poms/$i.pom -Dpackaging=zip -Dclassifier=$bpa
+fi
