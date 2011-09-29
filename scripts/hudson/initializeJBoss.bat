f9df2965d1095eecc98e290743f5f3e974d7a078
Tom Jenkinson <tom.jenkinson@redhat.com>
2011-09-29 15:27:57 +0000

    updated to download the dependency if it doesn't exist
    git-svn-id: http://anonsvn.jboss.org/repos/blacktie/trunk@3112 aa7494b7-e75f-0410-880c-f61b6724bf28

--- scripts/hudson/initializeJBoss.bat
+++ scripts/hudson/initializeJBoss.bat
@@ -2,25 +2,25 @@ rem CREATE A FILE TO DOWNLOAD JBOSS AND ORACLE DRIVER
 if exist build.xml del build.xml
 echo ^<project name="blacktie-dependencies"^> >> build.xml
 echo    ^<target name="download"^> >> build.xml
-rem echo        ^<get src="http://albany/userContent/blacktie/instantclient-basiclite-win32-11.2.0.1.0.zip" dest="./instantclient-basiclite-win32-11.2.0.1.0.zip" verbose="true" usetimestamp="true"/^> >> build.xml
-rem echo        ^<get src="http://albany/userContent/blacktie/instantclient-sdk-win32-11.2.0.1.0.zip" dest="./instantclient-sdk-win32-11.2.0.1.0.zip" verbose="true" usetimestamp="true"/^> >> build.xml
-rem echo        ^<delete dir="instantclient_11_2"/^> >> build.xml
-rem echo        ^<unzip src="./instantclient-basiclite-win32-11.2.0.1.0.zip" dest="."/^> >> build.xml
-rem echo        ^<unzip src="./instantclient-sdk-win32-11.2.0.1.0.zip" dest="."/^> >> build.xml
-rem echo        ^<mkdir dir="./instantclient_11_2/network/admin"/^> >> build.xml
-rem echo        ^<get src="http://albany/userContent/blacktie/tnsnames.ora" dest="./instantclient_11_2/network/admin/tnsnames.ora" verbose="true" usetimestamp="true"/^> >> build.xml
-rem echo        ^<move tofile="./instantclient_11_2/OCI" file="./instantclient_11_2/sdk"/^> >> build.xml
-rem echo        ^<move tofile="./instantclient_11_2/OCI/lib/MSVC2" file="./instantclient_11_2/OCI/lib/msvc"/^> >> build.xml
-rem echo        ^<move tofile="./instantclient_11_2/OCI/lib/MSVC" file="./instantclient_11_2/OCI/lib/MSVC2"/^> >> build.xml
-rem echo        ^<mkdir dir="./instantclient_11_2/bin"/^> >> build.xml
-rem echo        ^<move todir="./instantclient_11_2/bin"^> >> build.xml
-rem echo            ^<fileset dir="./instantclient_11_2/"^> >> build.xml
-rem echo                ^<include name="*.dll"/^> >> build.xml
-rem echo            ^</fileset^> >> build.xml
-rem echo        ^</move^> >> build.xml
-rem echo        ^<get src="http://albany/userContent/blacktie/jboss-5.1.0.GA.zip" dest="./jboss-5.1.0.GA.zip" verbose="true" usetimestamp="true"/^> >> build.xml
-rem echo        ^<get src="http://albany/userContent/blacktie/hornetq-2.1.2.Final.zip" dest="./hornetq-2.1.2.Final.zip" verbose="true" usetimestamp="true"/^> >> build.xml
-rem echo        ^<get src="http://albany/userContent/blacktie/jbossesb-4.9.zip" dest="./jbossesb-4.9.zip" verbose="true" usetimestamp="true"/^> >> build.xml
+echo        ^<get src="http://albany/userContent/blacktie/instantclient-basiclite-win32-11.2.0.1.0.zip" dest="./instantclient-basiclite-win32-11.2.0.1.0.zip" verbose="true" usetimestamp="true" skipexisting="true"/^> >> build.xml
+echo        ^<get src="http://albany/userContent/blacktie/instantclient-sdk-win32-11.2.0.1.0.zip" dest="./instantclient-sdk-win32-11.2.0.1.0.zip" verbose="true" usetimestamp="true" skipexisting="true"/^> >> build.xml
+echo        ^<delete dir="instantclient_11_2"/^> >> build.xml
+echo        ^<unzip src="./instantclient-basiclite-win32-11.2.0.1.0.zip" dest="."/^> >> build.xml
+echo        ^<unzip src="./instantclient-sdk-win32-11.2.0.1.0.zip" dest="."/^> >> build.xml
+echo        ^<mkdir dir="./instantclient_11_2/network/admin"/^> >> build.xml
+echo        ^<get src="http://albany/userContent/blacktie/tnsnames.ora" dest="./instantclient_11_2/network/admin/tnsnames.ora" verbose="true" usetimestamp="true" skipexisting="true"/^> >> build.xml
+echo        ^<move tofile="./instantclient_11_2/OCI" file="./instantclient_11_2/sdk"/^> >> build.xml
+echo        ^<move tofile="./instantclient_11_2/OCI/lib/MSVC2" file="./instantclient_11_2/OCI/lib/msvc"/^> >> build.xml
+echo        ^<move tofile="./instantclient_11_2/OCI/lib/MSVC" file="./instantclient_11_2/OCI/lib/MSVC2"/^> >> build.xml
+echo        ^<mkdir dir="./instantclient_11_2/bin"/^> >> build.xml
+echo        ^<move todir="./instantclient_11_2/bin"^> >> build.xml
+echo            ^<fileset dir="./instantclient_11_2/"^> >> build.xml
+echo                ^<include name="*.dll"/^> >> build.xml
+echo            ^</fileset^> >> build.xml
+echo        ^</move^> >> build.xml
+echo        ^<get src="http://albany/userContent/blacktie/jboss-5.1.0.GA.zip" dest="./jboss-5.1.0.GA.zip" verbose="true" usetimestamp="true" skipexisting="true"/^> >> build.xml
+echo        ^<get src="http://albany/userContent/blacktie/hornetq-2.1.2.Final.zip" dest="./hornetq-2.1.2.Final.zip" verbose="true" usetimestamp="true" skipexisting="true"/^> >> build.xml
+echo        ^<get src="http://albany/userContent/blacktie/jbossesb-4.9.zip" dest="./jbossesb-4.9.zip" verbose="true" usetimestamp="true" skipexisting="true"/^> >> build.xml
 echo        ^<delete dir="jboss-5.1.0.GA"/^> >> build.xml
 echo        ^<unzip src="./jboss-5.1.0.GA.zip" dest="."/^> >> build.xml
 echo        ^<delete dir="hornetq-2.1.2.Final"/^> >> build.xml
